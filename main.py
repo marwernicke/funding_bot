f87a5a0dbc017cdf22e8a970c18376bba25d4bf7
Matias Wernicke <mwernicke@swci.com.ar>
2020-06-24 17:00:53 -0300

    hourly_cancel() had some major changes

--- main.py
+++ main.py
@@ -1,36 +1,14 @@
-import asyncio
 from datetime import datetime as dt
 import time
+import sched
+import asyncio
 #### my Files
 from user import user
+from client_functions import orders
 from keys import TEST_API_KEY, TEST_API_SECRET, API_KEY, API_SECRET
 from client_functions import listener
 
-def horly_cancel(user):
-    while True:
-        time.sleep(3600)# an hour
-        print("####################### {} ###########################".format(user.user_name))
-        print("____CREDITS")
-        for credit in user.credits:
-            print(user.credits[credit])
-        print("____OFFERS")
-        for offer in user.offers:
-            print(user.offers[offer])
-        print("____WALLETS")
-        for wallet in user.wallets:
-            print(user.wallets[wallet])
-        print("##################################################")
-        actual_time = dt.now()
-        print('### check_cancel_all_offers() called at {}'.format(actual_time))
-        asyncio.run(user.cancel_all_offers())
-
-def run(user):
-    @user.bfx.ws.on('all')
-    async def on_all(data):
-        await listener.all(data,user)
-
-    time.sleep(2)
-
+def print_user_data(user):
     print("####################### {} ###########################".format(user.user_name))
     print("____CREDITS")
     for credit in user.credits:
@@ -40,14 +18,32 @@ def run(user):
         print(user.offers[offer])
     print("____WALLETS")
     for wallet in user.wallets:
-        print(wallet,user.wallets[wallet])
+        print(user.wallets[wallet])
     print("##################################################")
 
-    #horly_cancel(user)
+def horly_cancel(loop):
+    actual_time = dt.now()
+    print('### cancel_all_offers() called at {}'.format(actual_time))
+    for user in users:
+        print_user_data(user)
+        asyncio.run(orders.cancel_all_offers(user))
+    loop.enter(60, 1, horly_cancel, (loop,))
 
+def run(user):
+    @user.bfx.ws.on('all')
+    async def on_all(data):
+        await listener.all(data,user)
+    time.sleep(5) # time to load
+    print_user_data(user)
 
+########### Running starts here ###########
 test = user('Test_758',TEST_API_KEY, TEST_API_SECRET,['USD'])
-run(test)
-
 marcos = user('Mars_859',API_KEY,API_SECRET,['USD'])
-run(marcos)
+users = [marcos,test]
+for user in users:
+    run(user)
+
+# runs loop for hourly cancel
+loop = sched.scheduler(time.time, time.sleep)
+loop.enter(60, 1, horly_cancel, (loop,))
+loop.run()
